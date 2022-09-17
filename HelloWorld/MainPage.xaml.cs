fdb457ded448c1afbbbefcda4d0875891e734dae
oluwafemi13 <sundayjabikem@gmail.com>
2022-09-17 16:14:30 +0100

    Add project files.

---
+++ HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(object sender, EventArgs e)
+        {
+            string input = nameEntry.Text;
+            nameEntry.Text = " ";
+            nameDisplay.Text = input;
+
+        }
+    }
+}
